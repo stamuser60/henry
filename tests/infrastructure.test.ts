c2f41183422a9ce35b127b49c5e6dedbed2ecc08
Hyaxia <hyaxia@gmail.com>
2020-05-23 21:50:49 +0300

    Added tests for database, they are not complete yet

--- tests/infrastructure.test.ts
+++ tests/infrastructure.test.ts
@@ -9,6 +9,7 @@ import {
 } from './testConfig';
 import { CprLogger } from '@stamscope/jslogger';
 import sinon, { SinonSandbox } from 'sinon';
+import { createConnection, getConnection, Connection } from 'typeorm';
 import { expect } from 'chai';
 import { Readable, ReadableOptions } from 'stream';
 import logger from '../src/logger';
@@ -16,8 +17,13 @@ import { ConsumerGroupStream, Message, Producer } from 'kafka-node';
 import { KafkaEnrichmentDispatcher } from '../src/infrastructure/kafkaDispatcher';
 import { KafkaEnrichmentConsumer } from '../src/infrastructure/kafkaConsumer';
 import { Incident } from '../src/core/dataItem';
+import { incidentRepo } from '../src/infrastructure/sql/incidentRepo';
 import { AlertSchema, HermeticitySchema } from '../src/infrastructure/schemaGenerator';
 import { Schema, Validator } from 'jsonschema';
+import { HermeticityStatus, hermeticityType, ProcessedHermeticityEnrichment } from '../src/core/hermeticity';
+import { SqlAlert } from '../src/infrastructure/sql/sqlAlert';
+import { alertType, ProcessedAlertEnrichment, Severity } from '../src/core/alert';
+import { SqlHermeticity } from '../src/infrastructure/sql/sqlHermeticity';
 
 /**
  * A class to create test readable streams,
@@ -173,6 +179,325 @@ describe('Infrastructure', function() {
           return;
         }
         done('No error thrown');
+      });
+    });
+  });
+  describe('Typeorm', function() {
+    let connection: Connection;
+    const alert: ProcessedAlertEnrichment = {
+      timestamp: '2020-03-25T12:00:00Z',
+      origin: 'origin',
+      node: 'node',
+      type: alertType,
+      severity: Severity.minor,
+      ID: 'ID16',
+      description: 'description',
+      object: 'object',
+      application: 'application',
+      operator: 'operator'
+    };
+
+    const hermeticity: ProcessedHermeticityEnrichment = {
+      timestamp: '2020-03-25T12:00:00Z',
+      origin: 'origin',
+      ID: 'ID129',
+      type: hermeticityType,
+      value: 100,
+      beakID: 'beakID',
+      status: HermeticityStatus.critical,
+      hasAlert: true
+    };
+    beforeEach(function() {
+      return new Promise(resolve => {
+        createConnection().then(connectionToDB => {
+          console.log('before each');
+          connection = connectionToDB;
+          resolve();
+        });
+      });
+    });
+    afterEach(function() {
+      return new Promise(resolve => {
+        console.log('afterEach');
+        connection.close().then(connectionToDB => {
+          resolve();
+        });
+      });
+    });
+    describe('Alert', function() {
+      it('in Alert, showd get to insert timestampCreated and timestampMPP as string and insert as DATE ', async function() {
+        await incidentRepo.addAlert(alert);
+        const findAlert = (await connection
+          .getRepository(SqlAlert)
+          .createQueryBuilder('SqlAlert')
+          .where('id = :id', { id: alert.ID })
+          .getOne()) as SqlAlert;
+        await getConnection()
+          .createQueryBuilder()
+          .delete()
+          .from(SqlAlert)
+          .where('id = :id', { id: alert.ID })
+          .execute();
+        expect(Object.prototype.toString.call(findAlert.timestamp)).to.be.eq('[object Date]');
+      });
+
+      it('in Alert, if whele inserting the db is down the error is "sqlretrayerror ', async function() {
+        let errorStatus = 0;
+
+        await connection.close();
+        try {
+          await incidentRepo.addAlert(alert);
+        } catch (error) {
+          errorStatus = error.status;
+        }
+        connection = await createConnection();
+        expect(errorStatus).to.be.eq(1);
+      });
+
+      it('in Alert, if whele inserting the db is so slow the insert is failing the error is "sqlretrayerror ', async function() {
+        let errorStatus = 0;
+        const connectionForInsert = await createConnection({
+          name: 'connectionForInsert',
+          type: 'mssql',
+          host: 'localhost',
+          port: 49672,
+          username: 'login2',
+          password: 'login2',
+          database: 'emiDB',
+          entities: [SqlAlert]
+        });
+        const queryRunner = connectionForInsert.createQueryRunner();
+        await queryRunner.connect();
+        await queryRunner.startTransaction();
+        try {
+          await queryRunner.query('SELECT * FROM alert WITH (TABLOCK, HOLDLOCK)');
+          try {
+            await incidentRepo.addAlert(alert);
+          } catch (error) {
+            errorStatus = error.status;
+          }
+          await queryRunner.commitTransaction();
+        } catch (err) {
+          await queryRunner.rollbackTransaction();
+        } finally {
+          await queryRunner.release();
+        }
+        await connectionForInsert.close();
+        expect(errorStatus).to.be.eq(1);
+      });
+
+      it('in Alert, if whele inserting the ProcessedAlertEnrichment dont have the right stachar we will get "sqlfatalerror" ', async function() {
+        let errorStatus = 0;
+        const alertWitoutNode = {
+          origin: 'origin',
+          type: alertType,
+          node: 'node',
+          severity: Severity.minor,
+          ID: 'ID15',
+          description: 'description',
+          object: 'object',
+          application: 'application',
+          operator: 'operator'
+        };
+        try {
+          await incidentRepo.addAlert(alertWitoutNode as ProcessedAlertEnrichment);
+        } catch (error) {
+          errorStatus = error.status;
+        }
+
+        expect(errorStatus).to.be.eq(2);
+      });
+      it('in Alert, if whele inserting the ProcessedAlertEnrichment bad values we will get "sqlfatalerror" ', async function() {
+        let errorStatus = 0;
+        const alertWitoutNode = {
+          timestamp: '2020123-03-25T12:00:00Z',
+          origin: 'origin',
+          type: alertType,
+          node: 'node',
+          severity: Severity.minor,
+          ID: 'ID15',
+          description: 'description',
+          object: 'object',
+          application: 'application',
+          operator: 'operator'
+        };
+        try {
+          await incidentRepo.addAlert(alertWitoutNode as ProcessedAlertEnrichment);
+        } catch (error) {
+          errorStatus = error.status;
+        }
+
+        expect(errorStatus).to.be.eq(2);
+      });
+    });
+    describe('Hermeticity', function() {
+      it('in Hermeticity, showd get to insert timestampCreated and timestampMPP as string and insert as DATE ', async function() {
+        await incidentRepo.addHermeticity(hermeticity);
+        const findHermeticity = (await connection
+          .getRepository(SqlHermeticity)
+          .createQueryBuilder('SqlHermeticity')
+          .where('id = :id', { id: hermeticity.ID })
+          .getOne()) as SqlHermeticity;
+        await getConnection()
+          .createQueryBuilder()
+          .delete()
+          .from(SqlHermeticity)
+          .where('id = :id', { id: hermeticity.ID })
+          .execute();
+        expect(Object.prototype.toString.call(findHermeticity.timestamp)).to.be.eq('[object Date]');
+      });
+
+      it('in Hermeticity, showd get to insert hasAlert as boolean and insert as string ', async function() {
+        await incidentRepo.addHermeticity(hermeticity);
+        const findHermeticity = (await connection
+          .getRepository(SqlHermeticity)
+          .createQueryBuilder('SqlHermeticity')
+          .where('id = :id', { id: hermeticity.ID })
+          .getOne()) as SqlHermeticity;
+        await getConnection()
+          .createQueryBuilder()
+          .delete()
+          .from(SqlHermeticity)
+          .where('id = :id', { id: hermeticity.ID })
+          .execute();
+        expect(typeof findHermeticity.hasAlert).to.be.eq('string');
+      });
+
+      it('in Hermeticity, if whele inserting the db is down the error is "sqlretrayerror ', async function() {
+        let errorStatus = 0;
+
+        await connection.close();
+        try {
+          await incidentRepo.addHermeticity(hermeticity);
+        } catch (error) {
+          errorStatus = error.status;
+        }
+        connection = await createConnection();
+        expect(errorStatus).to.be.eq(1);
+      });
+
+      it('in Hermeticity, if whele inserting the db is so slow the insert is failing the error is "sqlretrayerror ', async function() {
+        let errorStatus = 0;
+        const connectionForInsert = await createConnection({
+          name: 'connectionForInsert',
+          type: 'mssql',
+          host: 'localhost',
+          port: 49672,
+          username: 'login2',
+          password: 'login2',
+          database: 'emiDB',
+          entities: [SqlHermeticity]
+        });
+        const queryRunner = connectionForInsert.createQueryRunner();
+        await queryRunner.connect();
+        await queryRunner.startTransaction();
+        try {
+          await queryRunner.query('SELECT * FROM Hermeticity WITH (TABLOCK, HOLDLOCK)');
+          try {
+            await incidentRepo.addHermeticity(hermeticity);
+          } catch (error) {
+            errorStatus = error.status;
+          }
+          await queryRunner.commitTransaction();
+        } catch (err) {
+          await queryRunner.rollbackTransaction();
+        } finally {
+          await queryRunner.release();
+        }
+        connectionForInsert.close();
+        expect(errorStatus).to.be.eq(1);
+      });
+
+      it('in Hermeticity, if whele inserting the ProcessedHermeticityEnrichment dont have the right stachar we will get "sqlfatalerror" ', async function() {
+        let errorStatus = 0;
+        const hermeticityWitoutTimestampCreated = {
+          origin: 'origin',
+          ID: 'ID129',
+          type: hermeticityType,
+          value: 100,
+          beakID: 'beakID',
+          status: HermeticityStatus.critical,
+          hasAlert: true
+        };
+        try {
+          await incidentRepo.addHermeticity(hermeticityWitoutTimestampCreated as ProcessedHermeticityEnrichment);
+        } catch (error) {
+          errorStatus = error.status;
+        }
+
+        expect(errorStatus).to.be.eq(2);
+      });
+
+      it('in Hermeticity, if whele inserting the ProcessedAlertEnrichment bad values we will get "sqlfatalerror" ', async function() {
+        let errorStatus = 0;
+        const hermeticityWitoutTimestampCreated = {
+          timestamp: '2020223-03-25T12:00:00Z',
+          origin: 'origin',
+          ID: 'ID129',
+          type: hermeticityType,
+          value: 100,
+          beakID: 'beakID',
+          status: HermeticityStatus.critical,
+          hasAlert: true
+        };
+        try {
+          await incidentRepo.addHermeticity(hermeticityWitoutTimestampCreated as ProcessedHermeticityEnrichment);
+        } catch (error) {
+          errorStatus = error.status;
+        }
+
+        expect(errorStatus).to.be.eq(2);
+      });
+    });
+
+    describe('getAllEnrichment', function() {
+      it('in getAllEnrichment, if whele selecting the db is down the error is "sqlretrayerror ', async function() {
+        let errorStatus = 0;
+
+        await connection.close();
+        try {
+          await incidentRepo.getAllEnrichment();
+        } catch (error) {
+          errorStatus = error.status;
+        }
+        connection = await createConnection();
+        expect(errorStatus).to.be.eq(1);
+      });
+
+      it('in getAllEnrichment, checing thst the stacter thet returns from the faunction is in the correct srachar', async function() {
+        let errorStatus = 0;
+        const connectionForInsert = await createConnection({
+          name: 'connectionForInsert',
+          type: 'mssql',
+          host: 'localhost',
+          port: 49672,
+          username: 'login2',
+          password: 'login2',
+          database: 'emiDB',
+          entities: [SqlHermeticity]
+        });
+        const queryRunner = connection.createQueryRunner();
+        let AllEnrichment;
+        await queryRunner.connect();
+        await queryRunner.startTransaction();
+        try {
+          await getConnection()
+            .createQueryBuilder()
+            .delete()
+            .from(SqlAlert)
+            .execute();
+          try {
+            AllEnrichment = await incidentRepo.getAllEnrichment();
+          } catch (error) {
+            errorStatus = error.status;
+          }
+          await queryRunner.rollbackTransaction();
+        } catch (err) {
+          await queryRunner.rollbackTransaction();
+        } finally {
+          await queryRunner.release();
+        }
+        connectionForInsert.close();
       });
     });
   });
